82b05f2c7e46003f00809a01d40ff7801e3e35c8
OfrenDialsa <ofrendialsa.dev@gmail.com>
2025-09-29 04:21:30 +0700

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
@@ -23,11 +24,17 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    /**
+     * Relasi ke Owner
+     */
     public function owner()
     {
         return $this->hasOne(Owner::class);
     }
 
+    /**
+     * Relasi ke Customer
+     */
     public function customer()
     {
         return $this->hasOne(Customer::class);
