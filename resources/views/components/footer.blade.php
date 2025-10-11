2c2ef716ebec228eb2f0735139f36584e278f6d2
OfrenDialsa <ofrendialsa.dev@gmail.com>
2025-10-11 23:33:26 +0700

    minor fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,5 @@
 <footer class="bg-white border-t">
+    <!-- Main Footer -->
     <div class="text-center py-8 border-t">
         <h3 class="text-lg font-semibold text-red-600">MejaKu</h3>
         <ul class="mt-3 space-y-1 text-gray-700">
@@ -7,6 +8,7 @@
             <li><a href="#" class="hover:text-red-600">Tentang</a></li>
         </ul>
 
+        <!-- Social Icons -->
         <div class="flex justify-center space-x-5 mt-5 text-gray-600">
             <a href="#"><i class="fab fa-facebook text-xl hover:text-red-600"></i></a>
             <a href="#"><i class="fab fa-instagram text-xl hover:text-red-600"></i></a>
@@ -15,6 +17,7 @@
         </div>
     </div>
 
+    <!-- Bottom Bar -->
     <div class="border-t text-center py-4 text-sm text-gray-500">
         <div class="flex justify-center space-x-6 mb-2">
             <a href="#" class="hover:text-red-600">Indonesia</a>
